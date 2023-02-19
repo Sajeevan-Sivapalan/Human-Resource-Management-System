cc97406f522635a9d6fcd97370974482e0d34a37
Sajeevan2001 <sajeesiva12@gmail.com>
2023-02-19 08:06:56 +0530

    add qr reader

--- client/src/components/AdminComponents/AdminContent.js
+++ client/src/components/AdminComponents/AdminContent.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import AdminReqLeave from "./AdminReqLeave";
 import Attendance from "./Attendance";
+import ReadQR from "./ReadQR";
 
 function AdminContent() {
     return(
@@ -10,6 +11,7 @@ function AdminContent() {
                 <Route>
                     <Route path="/AdminReqLeave" element={<AdminReqLeave />} />
                     <Route path="/Attendance" element={<Attendance />} />
+                    <Route path="/readQR" element={<ReadQR />} />
                 </Route>
             </Routes>
         </div>
--- client/src/components/AdminComponents/AdminSideBar.js
+++ client/src/components/AdminComponents/AdminSideBar.js
@@ -5,7 +5,7 @@ function AdminSideBar() {
     return(
         <div class="nav flex-column nav-pills bg-light sidebar" id="v-pills-tab" role="tablist" aria-orientation="vertical">
             <Link class="nav-link" to="">Home</Link>
-            <Link class="nav-link" to="">Profile</Link>
+            <Link class="nav-link" to="/readQR">Profile</Link>
             <Link class="nav-link" to="/Attendance">Attendance</Link>
             <Link class="nav-link" to="/AdminReqLeave">Leave Request</Link>
             
